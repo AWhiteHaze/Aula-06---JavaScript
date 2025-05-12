9beea1caed106f0ce39a61a7747922f922b0356a
AWhiteHaze <Flashgamesbr030@gmail.com>
2025-05-12 14:27:12 -0300

    Add files via upload

---
+++ func.js
@@ -0,0 +1,10 @@
+    
+function clicou1(){
+window.alert('Você escolheu a caixa de texto 1')
+}
+function clicou2(){
+window.alert('Você escolheu a caixa de texto 2')
+}
+function clicou3(){
+window.alert('Você escolheu a caixa de texto 3')
+}
